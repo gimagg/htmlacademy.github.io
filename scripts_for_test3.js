caf84ab105f594fdd0a7700e11f1adeb94f60e55
gimagg <dimasidorenko80@gmail.com>
2018-10-20 15:19:17 +0300

    Merge branch 'master' of https://github.com/gimagg/htmlacademy.github.io

--- scripts_for_test3.js
+++ scripts_for_test3.js
@@ -1,5 +1,5 @@
 function plane() {
-  var plane = '{"name": "plane", "time": "5", "capacity" : "7", "cost":"500"}';
+  var plane = '{"name": "Самолет №1", "time": "2", "capacity" : "12", "cost":"160"}';
 
    plane = JSON.parse(plane);
    $('#re1_tupe').text(plane.name);
@@ -7,7 +7,7 @@ function plane() {
    $('#re1_capacity').text(plane.capacity);
    $('#re1_time').text(plane.time);
 
-   var plane2 = '{"name": "plane", "time": "5", "capacity" : "7", "cost":"500"}';
+   var plane2 = '{"name": "Самолет №2", "time": "2", "capacity" : "8", "cost":"120"}';
 
     plane2 = JSON.parse(plane2);
     $('#re2_tupe').text(plane2.name);
@@ -15,7 +15,7 @@ function plane() {
     $('#re2_capacity').text(plane2.capacity);
     $('#re2_time').text(plane2.time);
 
-    var plane3 = '{"name": "plane", "time": "5", "capacity" : "7", "cost":"500"}';
+    var plane3 = '{"name": "Самолет №3", "time": "3", "capacity" : "11", "cost":"130"}';
 
      plane3 = JSON.parse(plane3);
      $('#re3_tupe').text(plane3.name);
@@ -25,21 +25,21 @@ function plane() {
 
  }
  function ship() {
-   var ship = '{"name": "pla", "time": "5", "capacity" : "7", "cost":"500"}';
+   var ship = '{"name": "Катер №1", "time": "7", "capacity" : "8", "cost":"125"}';
     ship = JSON.parse(ship);
     $('#re1_tupe').text(ship.name);
     $('#re1_cost').text(ship.cost);
     $('#re1_capacity').text(ship.capacity);
     $('#re1_time').text(ship.time);
 
-    var ship2 = '{"name": "pla", "time": "5", "capacity" : "7", "cost":"500"}';
+    var ship2 = '{"name": "Катер №2", "time": "7", "capacity" : "9", "cost":"145"}';
      ship2 = JSON.parse(ship2);
      $('#re2_tupe').text(ship2.name);
      $('#re2_cost').text(ship2.cost);
      $('#re2_capacity').text(ship2.capacity);
      $('#re2_time').text(ship2.time);
 
-     var ship3 = '{"name": "pla", "time": "5", "capacity" : "7", "cost":"500"}';
+     var ship3 = '{"name": "Катер №3", "time": "6", "capacity" : "8", "cost":"155"}';
         ship3 = JSON.parse(ship3);
         $('#re3_tupe').text(ship3.name);
         $('#re3_cost').text(ship3.cost);
@@ -48,19 +48,20 @@ function plane() {
 
   }
   function helicopter() {
-    var helicopter = '{"name": "руд", "time": "5", "capacity" : "7", "cost":"500"}';
-JSON.parse(helicopter);
+    var helicopter = '{"name": "Вертолет №1", "time": "2", "capacity" : "6", "cost":"170"}';
+    helicopter = JSON.parse(helicopter);
      $('#re1_tupe').text(helicopter.name);
      $('#re1_cost').text(helicopter.cost);
      $('#re1_capacity').text(helicopter.capacity);
      $('#re1_time').text(helicopter.time);
 
-     var helicopter2 = '{"name": "руд", "time": "5", "capacity" : "7", "cost":"500"}';
+     var helicopter2 = '{"name": "Вертолет №2 ", "time": "2", "capacity" : "5", "cost":"150"}';
+     helicopter2 = JSON.parse(helicopter2);
      $('#re2_tupe').text(helicopter2.name);
       $('#re2_cost').text(helicopter2.cost);
       $('#re2_capacity').text(helicopter2.capacity);
       $('#re2_time').text(helicopter2.time);
-      var helicopter3 = '{"name": "руд", "time": "5", "capacity" : "7000", "cosе":"500"}';
+      var helicopter3 = '{"name": "Вертолет №3", "time": "3", "capacity" : "5", "cost":"200"}';
            helicopter3 = JSON.parse(helicopter3);
          $('#re3_tupe').text(helicopter3.name);
          $('#re3_cost').text(helicopter3.cost);
