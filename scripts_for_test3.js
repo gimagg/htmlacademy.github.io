1132319ea676a9c15ad38bfb8b5940451ec8a391
gimagg <dimasidorenko80@gmail.com>
2018-10-20 13:44:31 +0300

    Merge branch 'master' of https://github.com/gimagg/htmlacademy.github.io

--- scripts_for_test3.js
+++ scripts_for_test3.js
@@ -1,6 +1,26 @@
 function plane() {
-  var b = '{"name": "plane", "time": "5", "cop" : "7", "coins":"500"}';
-   b = JSON.parse(b);
-   $('#re1_tupe').text(b.name);
-   $('#re1_stoi').text(b.coins);
+  var plane = '{"name": "plane", "time": "5", "capacity" : "7", "cost":"500"}';
+
+   plane = JSON.parse(plane);
+   $('#re1_tupe').text(plane.name);
+   $('#re1_cost').text(plane.cost);
+   $('#re1_capacity').text(plane.capacity);
+   $('#re1_time').text(plane.time);
  }
+ function ship() {
+   var ship = '{"name": "pla", "time": "5", "capacity" : "7", "cost":"500"}';
+    ship = JSON.parse(ship);
+    $('#re1_tupe').text(ship.name);
+    $('#re1_cost').text(ship.cost);
+    $('#re1_capacity').text(ship.capacity);
+    $('#re1_time').text(ship.time);
+
+  }
+  function helicopter() {
+    var helicopter = '{"name": "p", "time": "5", "capacity" : "7", "cost":"500"}';
+     helicopter = JSON.parse(helicopter);
+     $('#re1_tupe').text(helicopter.name);
+     $('#re1_cost').text(helicopter.cost);
+     $('#re1_capacity').text(helicopter.capacity);
+     $('#re1_time').text(helicopter.time);
+   }
